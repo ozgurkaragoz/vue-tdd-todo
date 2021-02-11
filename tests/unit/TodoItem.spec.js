b2129fc01624c7076d2d403312c33929a6e03c86
Ozgur KARAGOZ <ozgur.karagoz@modanisa.com>
2021-02-11 15:36:53 +0300

    Add TodoItem component's tests.

---
+++ tests/unit/TodoItem.spec.js
@@ -0,0 +1,26 @@
+import {shallowMount} from '@vue/test-utils'
+import TodoItem from '@/components/TodoItem.vue'
+
+describe('TodoItem.vue', () => {
+    it('should render component', () => {
+        const wrapper = shallowMount(TodoItem, {
+            propsData: {
+                item: {
+                    text: "Deneme 1"
+                }
+            }
+        })
+        expect(wrapper.exists()).toBe(true)
+    })
+
+    it('should render props data successfully', () => {
+        const wrapper = shallowMount(TodoItem, {
+            propsData: {
+                item: {
+                    text: "Deneme 1"
+                }
+            }
+        })
+        expect(wrapper.text()).toBe("Deneme 1")
+    })
+})
